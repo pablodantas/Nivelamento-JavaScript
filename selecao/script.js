93de3bd212ce53be57312c48e3997f87f759af29
Pablo Dantas <pablodesouzadantas@gmail.com>
2022-01-20 21:03:17 -0300

    Introdução à manipulação de DOM

---
+++ selecao/script.js
@@ -0,0 +1,30 @@
+/*
+const item1 = document.querySelector("li");
+
+console.log(item1);
+console.log(item1.outerHTML);
+console.log(item1.innerHTML);
+
+const items = document.querySelectorAll("li");
+
+console.log(items);
+
+const card1 = document.querySelector('.card')
+
+console.log(card1);
+
+const paragrafo = document.createElement("p");
+paragrafo.innerHTML = "Descrição";
+card1.appendChild(paragrafo);
+
+card1.classList.add("super-border");
+
+const cards =document.querySelectorAll(".card")
+
+Array.from(cards).map(item =>{
+    console.log(item.innerHTML);
+});
+*/
+const card1 = document.querySelector(`[data-product-id="32"]`);
+
+console.log(card1);
